7dd0eaef869bb30eb29953790a55bb47c3d643e7
Your Name <you@example.com>
2025-06-12 19:15:34 -0400

    Describe brevemente los cambios realizados

---
+++ main.c
@@ -0,0 +1,13 @@
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+#include    <ctype.h>
+#include    <stdbool.h>
+#include    <unistd.h>
+
+//funcion que imprime el mensaje de bienvenida
+void print_welcome_message() {
+    printf("Bienvenido al programa de ejemplo.\n");
+    printf("Este programa demuestra el uso de funciones en C.\n");
+}
+
