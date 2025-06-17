e0dbc7caff7d2e4132f847f7de50c896fb4cf7a0
Your Name <you@example.com>
2025-06-16 20:17:26 -0400

    cambio de tda list

--- tdas/list.h
+++ tdas/list.h
@@ -1,49 +1,41 @@
 #ifndef LIST_H
 #define LIST_H
 
-typedef struct Node {
-    void *data;
-    struct Node *next;
-    struct Node *prev;
-} Node;
+typedef struct Node Node;
 
-typedef struct List {
-    Node *head;
-    Node *tail;
-    Node *current;
-    int size;
-} List;
-
+typedef struct List List;
 
 // Esta función crea una lista vacía y devuelve un puntero a la lista.
-List *create_list();
-
-void imprime_lista(List *L); // Esta función imprime la lista en formato [a b c d e]
+List *list_create();
 
 // Esta función devuelve un puntero al primer elemento de la lista.
-void *first(List *L);
+void *list_first(List *L);
 
 // Esta función mueve el puntero de la lista al siguiente elemento y devuelve un
 // puntero a dicho elemento.
-void *next(List *L);
+void *list_next(List *L);
+
+// Esta función mueve el puntero de la lista al elemento anterior y devuelve un
+// puntero a dicho elemento.
+void *list_prev(List *L);
 
 // Esta función inserta un nuevo elemento al inicio de la lista.
-void pushFront(List *L, void *dato);
+void list_pushFront(List *L, void *dato);
 
 // Esta función inserta un nuevo elemento al final de la lista.
-void pushBack(List *L, void *dato);
+void list_pushBack(List *L, void *dato);
 
 // Esta función inserta un nuevo elemento a continuación del actual de la lista.
-void pushCurrent(List *L, void *dato);
+void list_pushCurrent(List *L, void *dato);
 
 // Esta función elimina el primer elemento de la lista.
-void *popFront(List *L);
+void *list_popFront(List *L);
 
 // Esta función elimina el último elemento de la lista.
-void *popBack(List *L);
+void *list_popBack(List *L);
 
 // Esta función elimina el elemento actual de la lista.
-void *popCurrent(List *L);
+void *list_popCurrent(List *L);
 
 // Esta función elimina todos los elementos de la lista.
 void list_clean(List *L);
